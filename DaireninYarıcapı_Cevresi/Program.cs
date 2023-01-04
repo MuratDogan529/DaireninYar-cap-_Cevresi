2ece9d8c8b2b824360e1f1b9da01718f174158dc
murat.dogan2 <murat.dogan2@ERKHBN0096>
2023-01-04 16:38:47 +0300

    Add project files.

---
+++ DaireninYarıcapı_Cevresi/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+const double PI = 3.14;
+double yaricap, alan, cevre;
+Console.Write("Dairenin Yarıçapı:");
+yaricap =Convert.ToDouble(Console.ReadLine());
+alan = PI * yaricap * yaricap;
+cevre = 2 * PI * yaricap;
+
+Console.WriteLine("Dairenin Alanı:"+alan);
+Console.WriteLine("Dairenin Çevresi:"+cevre);
